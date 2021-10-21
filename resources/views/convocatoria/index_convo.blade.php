e32b471c67dc2904ce688933abca851f905d2819
fabricio <fabricio.alborta.22@gmail.com>
2021-10-21 17:16:19 -0400

    plantilla index convocatoria

---
+++ resources/views/convocatoria/index_convo.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.plantilla_convo')
+
+@section('contenido')
+<h1>Vista index</h1>
+@endsection
--- resources/views/layouts/plantilla_convo.blade.php
+++ resources/views/layouts/plantilla_convo.blade.php
@@ -8,10 +8,13 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-    <title>Hello, world!</title>
+    <title>Registro de Grupo - Empresas </title>
   </head>
   <body>
-    <h1>Hello, world!</h1>
+    <h1 class="text-center text-white bg-primary">REGISTRO DE GRUPO - EMPRESAS</h1>
+    <div class="container">
+        @yield('contenido')
+    </div>
 
     <!-- Optional JavaScript; choose one of the two! -->
 
