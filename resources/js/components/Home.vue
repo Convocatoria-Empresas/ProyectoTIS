3ccff689f0ccd6a7933fb67fdbbfb723ed6d6f2b
fabricio <fabricio.alborta.22@gmail.com>
2021-11-30 15:05:39 -0400

    actualizo el home para los asesores

--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -16,7 +16,6 @@
         <h2>Asesores Destacados</h2>
         <hr>
     </div>
-
     <div class="row">
       <div class="col-lg-4">
         <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777" data-darkreader-inline-fill="" style="--darkreader-inline-fill:#5a6165;"></rect><text x="50%" y="50%" fill="#777" dy=".3em" data-darkreader-inline-fill="" style="--darkreader-inline-fill:#9d9488;">140x140</text></svg>
