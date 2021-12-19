01102403447cb74e2c78ddc087acaeb8f150a1b8
fabricio <fabricio.alborta.22@gmail.com>
2021-12-18 20:52:22 -0400

    titulos y pagina estructurada un 20%

--- resources/js/components/Seguimiento/RevSegEmpre.vue
+++ resources/js/components/Seguimiento/RevSegEmpre.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="text-center col">
-        <h1>Seguimiento para las Grupo-Empresas</h1>
+        <h1>Revision de la Grupo-Empresas</h1>
         <hr>
-        
+
     </div>
 </template>
---
+++ resources/js/components/Seguimiento/RevSegPost.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="text-center col">
+        <h1>Revision de los Postulantes</h1>
+        <hr>
+
+    </div>
+</template>
