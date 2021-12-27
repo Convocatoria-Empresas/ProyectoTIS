d8822d443f166281281883bac6548de346475617
fabricio <fabricio.alborta.22@gmail.com>
2021-12-26 23:26:31 -0400

    La revision de la empresa

--- public/js/resources_js_components_Seguimiento_RevSegEmpre_vue.js
+++ public/js/resources_js_components_Seguimiento_RevSegEmpre_vue.js
@@ -68,7 +68,27 @@ var render = function () {
   var _vm = this
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
-  return _vm._m(0)
+  return _c("div", { staticClass: "row" }, [
+    _vm._m(0),
+    _vm._v(" "),
+    _c("div", { staticClass: "col-12" }, [
+      _c("div", { staticClass: "card-body" }, [
+        _c(
+          "form",
+          {
+            attrs: { action: "" },
+            on: {
+              submit: function ($event) {
+                $event.preventDefault()
+                return _vm.RevSegEmpre.apply(null, arguments)
+              },
+            },
+          },
+          [_vm._m(1)]
+        ),
+      ]),
+    ]),
+  ])
 }
 var staticRenderFns = [
   function () {
@@ -79,6 +99,236 @@ var staticRenderFns = [
       _c("h1", [_vm._v("Revision de la Grupo-Empresas")]),
       _vm._v(" "),
       _c("hr"),
+    ])
+  },
+  function () {
+    var _vm = this
+    var _h = _vm.$createElement
+    var _c = _vm._self._c || _h
+    return _c("div", { staticClass: "col-lg-5 offset-md-4" }, [
+      _c("form", [
+        _c("fieldset", { attrs: { disabled: "" } }, [
+          _c("div", { staticClass: "form-group" }, [
+            _c("label", { attrs: { for: "disabledTextInput" } }, [
+              _c("i", { staticClass: "fas fa-calendar-week" }),
+              _vm._v(" Gestión"),
+            ]),
+            _vm._v(" "),
+            _c("input", {
+              staticClass: "form-control",
+              attrs: {
+                type: "text",
+                id: "disabledTextInput",
+                placeholder: "Gestión",
+              },
+            }),
+          ]),
+          _vm._v(" "),
+          _c("div", { staticClass: "form-group" }, [
+            _c("label", { attrs: { for: "disabledTextInput" } }, [
+              _c("i", { staticClass: "far fa-user" }),
+              _vm._v(" Asesor"),
+            ]),
+            _vm._v(" "),
+            _c("input", {
+              staticClass: "form-control",
+              attrs: {
+                type: "text",
+                id: "disabledTextInput",
+                placeholder: "Nombre del Asesor",
+              },
+            }),
+          ]),
+          _vm._v(" "),
+          _c("div", { staticClass: "form-group" }, [
+            _c("label", { attrs: { for: "disabledTextInput" } }, [
+              _c("i", { staticClass: "fas fa-signature" }),
+              _vm._v(" Nombre"),
+            ]),
+            _vm._v(" "),
+            _c("input", {
+              staticClass: "form-control",
+              attrs: {
+                type: "text",
+                id: "disabledTextInput",
+                placeholder: "Nombre de la Grupo Empresa",
+              },
+            }),
+          ]),
+          _vm._v(" "),
+          _c("div", { staticClass: "form-group" }, [
+            _c("label", { attrs: { for: "disabledTextInput" } }, [
+              _c("i", { staticClass: "fas fa-envelope-open-text" }),
+              _vm._v(" Correo Electrónico"),
+            ]),
+            _vm._v(" "),
+            _c("input", {
+              staticClass: "form-control",
+              attrs: {
+                type: "text",
+                id: "disabledTextInput",
+                placeholder: "Correo de la Empresa",
+              },
+            }),
+          ]),
+          _vm._v(" "),
+          _c("div", { staticClass: "form-group" }, [
+            _c("label", { attrs: { for: "disabledTextInput" } }, [
+              _c("i", { staticClass: "fas fa-envelope-open-text" }),
+              _vm._v(" Correo Electrónico"),
+            ]),
+            _vm._v(" "),
+            _c("input", {
+              staticClass: "form-control",
+              attrs: {
+                type: "text",
+                id: "disabledTextInput",
+                placeholder: "Correo de la Empresa",
+              },
+            }),
+            _vm._v(" "),
+            _c("hr"),
+          ]),
+          _vm._v(" "),
+          _c("div", { staticClass: "form-group" }, [
+            _c("label", { attrs: { for: "disabledTextInput" } }, [
+              _c("i", { staticClass: "fas fa-file-pdf" }),
+              _vm._v(" Boleta de garantia"),
+            ]),
+            _vm._v(" "),
+            _c("div", { staticClass: "input-group-prepend" }, [
+              _c(
+                "span",
+                {
+                  staticClass: "input-group-text",
+                  attrs: { id: "inputGroupPrepend" },
+                },
+                [_vm._v("PDF")]
+              ),
+            ]),
+            _vm._v(" "),
+            _c("input", {
+              staticClass: "form-control",
+              attrs: {
+                type: "text",
+                id: "validationCustomUsername",
+                placeholder: "Documento.pdf",
+                "aria-describedby": "inputGroupPrepend",
+                required: "",
+              },
+            }),
+          ]),
+          _vm._v(" "),
+          _c("div", { staticClass: "form-group" }, [
+            _c("label", { attrs: { for: "disabledTextInput" } }, [
+              _c("i", { staticClass: "fas fa-file-pdf" }),
+              _vm._v(" Carta de Presentación"),
+            ]),
+            _vm._v(" "),
+            _c("div", { staticClass: "input-group-prepend" }, [
+              _c(
+                "span",
+                {
+                  staticClass: "input-group-text",
+                  attrs: { id: "inputGroupPrepend" },
+                },
+                [_vm._v("PDF")]
+              ),
+            ]),
+            _vm._v(" "),
+            _c("input", {
+              staticClass: "form-control",
+              attrs: {
+                type: "text",
+                id: "validationCustomUsername",
+                placeholder: "Documento.pdf",
+                "aria-describedby": "inputGroupPrepend",
+                required: "",
+              },
+            }),
+          ]),
+          _vm._v(" "),
+          _c("div", { staticClass: "form-group" }, [
+            _c("label", { attrs: { for: "disabledTextInput" } }, [
+              _c("i", { staticClass: "fas fa-file-pdf" }),
+              _vm._v(" Constitución de la Empresa"),
+            ]),
+            _vm._v(" "),
+            _c("div", { staticClass: "input-group-prepend" }, [
+              _c(
+                "span",
+                {
+                  staticClass: "input-group-text",
+                  attrs: { id: "inputGroupPrepend" },
+                },
+                [_vm._v("PDF")]
+              ),
+            ]),
+            _vm._v(" "),
+            _c("input", {
+              staticClass: "form-control",
+              attrs: {
+                type: "text",
+                id: "validationCustomUsername",
+                placeholder: "Documento.pdf",
+                "aria-describedby": "inputGroupPrepend",
+                required: "",
+              },
+            }),
+          ]),
+          _vm._v(" "),
+          _c("div", { staticClass: "form-group" }, [
+            _c("label", { attrs: { for: "disabledTextInput" } }, [
+              _c("i", { staticClass: "fas fa-file-pdf" }),
+              _vm._v(" Solvencia Técnica"),
+            ]),
+            _vm._v(" "),
+            _c("div", { staticClass: "input-group-prepend" }, [
+              _c(
+                "span",
+                {
+                  staticClass: "input-group-text",
+                  attrs: { id: "inputGroupPrepend" },
+                },
+                [_vm._v("PDF")]
+              ),
+            ]),
+            _vm._v(" "),
+            _c("input", {
+              staticClass: "form-control",
+              attrs: {
+                type: "text",
+                id: "validationCustomUsername",
+                placeholder: "Documento.pdf",
+                "aria-describedby": "inputGroupPrepend",
+                required: "",
+              },
+            }),
+          ]),
+        ]),
+        _vm._v(" "),
+        _c("hr"),
+        _vm._v(" "),
+        _c("div", { staticClass: "form-group" }, [
+          _c("label", { attrs: { for: "exampleFormControlTextarea1" } }, [
+            _c("i", { staticClass: "fas fa-comments" }),
+            _vm._v(" Escribir las observaciones"),
+          ]),
+          _vm._v(" "),
+          _c("textarea", {
+            staticClass: "form-control",
+            attrs: { id: "exampleFormControlTextarea1", rows: "3" },
+          }),
+        ]),
+        _vm._v(" "),
+        _c(
+          "button",
+          { staticClass: "btn btn-success", attrs: { type: "submit" } },
+          [_vm._v("Enviar")]
+        ),
+        _vm._v(" "),
+        _c("button", { staticClass: "btn btn-warning" }, [_vm._v("Volver")]),
+      ]),
     ])
   },
 ]
--- resources/js/components/Seguimiento/RevSegEmpre.vue
+++ resources/js/components/Seguimiento/RevSegEmpre.vue
@@ -1,7 +1,76 @@
 <template>
-    <div class="text-center col">
+    <div class="row">
+        <div class="text-center col">
         <h1>Revision de la Grupo-Empresas</h1>
         <hr>
-
+    </div>
+    <div class="col-12">
+        <div class="card-body">
+            <form @submit.prevent="RevSegEmpre" action="">
+                <div class="col-lg-5 offset-md-4">
+                    <form>
+                        <fieldset disabled>
+                            <div class="form-group">
+                                <label for="disabledTextInput"><i class="fas fa-calendar-week"></i> Gestión</label>
+                                <input type="text" id="disabledTextInput" class="form-control" placeholder="Gestión">
+                            </div>
+                            <div class="form-group">
+                                <label for="disabledTextInput"><i class="far fa-user"></i> Asesor</label>
+                                <input type="text" id="disabledTextInput" class="form-control" placeholder="Nombre del Asesor">
+                            </div>
+                            <div class="form-group">
+                                <label for="disabledTextInput"><i class="fas fa-signature"></i> Nombre</label>
+                                <input type="text" id="disabledTextInput" class="form-control" placeholder="Nombre de la Grupo Empresa">
+                            </div>
+                            <div class="form-group">
+                                <label for="disabledTextInput"><i class="fas fa-envelope-open-text"></i> Correo Electrónico</label>
+                                <input type="text" id="disabledTextInput" class="form-control" placeholder="Correo de la Empresa">
+                            </div>
+                            <div class="form-group">
+                                <label for="disabledTextInput"><i class="fas fa-envelope-open-text"></i> Correo Electrónico</label>
+                                <input type="text" id="disabledTextInput" class="form-control" placeholder="Correo de la Empresa">
+                                <hr>
+                            </div>
+                            <div class="form-group">
+                                <label for="disabledTextInput"><i class="fas fa-file-pdf"></i> Boleta de garantia</label>
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text" id="inputGroupPrepend">PDF</span>
+                                </div>
+                                <input type="text" class="form-control" id="validationCustomUsername" placeholder="Documento.pdf" aria-describedby="inputGroupPrepend" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="disabledTextInput"><i class="fas fa-file-pdf"></i> Carta de Presentación</label>
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text" id="inputGroupPrepend">PDF</span>
+                                </div>
+                                <input type="text" class="form-control" id="validationCustomUsername" placeholder="Documento.pdf" aria-describedby="inputGroupPrepend" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="disabledTextInput"><i class="fas fa-file-pdf"></i> Constitución de la Empresa</label>
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text" id="inputGroupPrepend">PDF</span>
+                                </div>
+                                <input type="text" class="form-control" id="validationCustomUsername" placeholder="Documento.pdf" aria-describedby="inputGroupPrepend" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="disabledTextInput"><i class="fas fa-file-pdf"></i> Solvencia Técnica</label>
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text" id="inputGroupPrepend">PDF</span>
+                                </div>
+                                <input type="text" class="form-control" id="validationCustomUsername" placeholder="Documento.pdf" aria-describedby="inputGroupPrepend" required>
+                            </div>
+                        </fieldset>
+                        <hr>
+                        <div class="form-group">
+                            <label for="exampleFormControlTextarea1"><i class="fas fa-comments"></i> Escribir las observaciones</label>
+                            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                        </div>
+                        <button class="btn btn-success" type="submit">Enviar</button>
+                        <button class="btn btn-warning">Volver</button>
+                    </form>
+                </div>
+            </form>
+        </div>
+    </div>
     </div>
 </template>
